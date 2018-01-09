692a3d69784ee89243c628c21c7ab028eea83575
Guy Allard <w.g.allard@lumc.nl>
2018-01-09 11:09:59 +0100

    generator function yielding udp packets

--- f1_telemetry/telemetry.py
+++ f1_telemetry/telemetry.py
@@ -4,24 +4,39 @@ from structs import UDPPacket
 import ctypes
 
 
-def on_packet(packet):
-    print(int(packet.gear) - 1)
-
-
-def get_packet(port, address):
+def get_packet(address, port):
+    """
+    Recieve a single UDP telemetry packet from the specified port and ip address
+    
+    :param address: IP address for the socket
+    :param port: Port for the socket
+    :return: A UDPPacket
+    """
+    # create a socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # bind the socket to the specified ip address and port
     sock.bind((address, port))
+    # recieve data
     data, addr = sock.recvfrom(ctypes.sizeof(UDPPacket))
+    # convert from raw bytes to UDPPacket structure
     return UDPPacket.from_buffer_copy(data)
 
 
-def get_telemetry(address, port, callback=on_packet):
+def get_telemetry(address, port):
+    """
+    Generator function which yields UDPPackets from the specified ip address and port
+    
+    :param address: IP address for receiving packets
+    :param port: Port on which to receive packets
+    :yeild: a UDPPacket for each udp packet received
+    """
     last_packet = None
     while True:
-        packet = get_packet(port, address)
+        packet = get_packet(address, port)
         if last_packet is None or packet.time > last_packet.time:
-            callback(packet)
+            yield packet
             last_packet = packet
 
 
-get_telemetry('192.168.1.150', 20777)
+for packet in get_telemetry('192.168.1.150', 20777):
+    print(int(packet.gear) - 1)
